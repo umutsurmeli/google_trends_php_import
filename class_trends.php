d18163adaeacfe6a980599ff653a4fc64c502e1f
Umut Sürmeli <umutsurmeli@gmail.com>
2019-12-06 23:29:30 +0200

    google trends verisini kaydedeceğimiz "datatable" ı oluşturduk

---
+++ class_trends.php
@@ -0,0 +1,68 @@
+<?php
+class trends {
+	var $connection=false;
+	function baglan($host,$user,$password,$db) {
+		$con = new mysqli($host,$user,$password,$db);
+		if($con!==false) {
+			$this->connection = $con;
+			return $con;
+		}
+		else {
+			return false;
+		}
+		
+	}
+	/*
+	function create_channel() {
+		if($this->connection === false) {
+			exit('bağlantı yok');
+		}
+		$sql = 'CREATE TABLE channel (id int(11) unsigned NOT NULL,
+			title VARCHAR(255) NOT NULL,
+			PRIMARY KEY(id),
+			UNIQUE(title)) CHARSET=utf8 COLLATE utf8_general_ci ENGINE=InnoDB;';
+			//exit($sql);
+		$con = $this->connection;
+		//echo var_dump($con);exit();
+		if(($res=$con->query($sql))===false) {
+			echo $con->error;
+		}
+		else {
+			echo 'sorgu çalıştı';
+		}
+		
+	}
+        */
+        function create_datatable() {
+		if($this->connection === false) {
+			exit('bağlantı yok');
+		}
+		$sql = 'CREATE TABLE datatable (
+                        id INT(11) UNSIGNED NOT NULL,
+                        channel_title VARCHAR(255) NULL,
+                        channel_description VARCHAR(255) NULL,
+			item_title VARCHAR(255) NOT NULL,
+                        approx_traffic VARCHAR(20) NOT NULL,
+                        item_description VARCHAR(255) NOT NULL,
+                        item_pubDate VARCHAR(50) NOT NULL,
+                        item_picture VARCHAR(255) NULL,
+                        item_picture_source VARCHAR(255) NULL,
+                        news_title VARCHAR(255) NOT NULL,
+                        news_snippet VARCHAR(255) NOT NULL,
+                        news_url VARCHAR(255) NOT NULL,
+                        news_source VARCHAR(50) NOT NULL,
+                        tarih TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+			PRIMARY KEY(id)
+                        ) CHARSET=utf8 COLLATE utf8_general_ci ENGINE=InnoDB;';
+			//exit($sql);
+		$con = $this->connection;
+		//echo var_dump($con);exit();
+		if(($res=$con->query($sql))===false) {
+			echo $con->error;
+		}
+		else {
+			echo 'datatable oluşturuldu.';
+		}
+        }
+	
+}
---
+++ kurulum.php
@@ -0,0 +1,11 @@
+<?php
+$host='localhost';
+$user = 'root';
+$password = '';
+$db = 'trends';
+ini_set('display_errors',true);
+include('class_trends.php');
+$trends = new trends();
+$con=$trends->baglan($host,$user,$password,$db);
+//var_dump($con);
+$trends->create_datatable();
