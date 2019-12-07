1d99577c06f59637c44e5df4912ee96732c6c04f
Umut Sürmeli <umutsurmeli@gmail.com>
2019-12-07 04:38:13 +0300

    testx

--- class_trends.php
+++ class_trends.php
@@ -12,27 +12,7 @@ class trends {
 		}
 		
 	}
-	/*
-	function create_channel() {
-		if($this->connection === false) {
-			exit('bağlantı yok');
-		}
-		$sql = 'CREATE TABLE channel (id int(11) unsigned NOT NULL,
-			title VARCHAR(255) NOT NULL,
-			PRIMARY KEY(id),
-			UNIQUE(title)) CHARSET=utf8 COLLATE utf8_general_ci ENGINE=InnoDB;';
-			//exit($sql);
-		$con = $this->connection;
-		//echo var_dump($con);exit();
-		if(($res=$con->query($sql))===false) {
-			echo $con->error;
-		}
-		else {
-			echo 'sorgu çalıştı';
-		}
-		
-	}
-        */
+
         function create_datatable() {
 		if($this->connection === false) {
 			exit('bağlantı yok');
@@ -42,7 +22,7 @@ class trends {
                         channel_title VARCHAR(255) NULL,
                         channel_description VARCHAR(255) NULL,
 			item_title VARCHAR(255) NOT NULL,
-                        approx_traffic VARCHAR(20) NOT NULL,
+                        item_traffic VARCHAR(20) NOT NULL,
                         item_description VARCHAR(255) NOT NULL,
                         item_pubDate VARCHAR(50) NOT NULL,
                         item_picture VARCHAR(255) NULL,
@@ -64,5 +44,48 @@ class trends {
 			echo 'datatable oluşturuldu.';
 		}
         }
+
+        function show_create_datatable() {
+            $sql = 'SHOW CREATE TABLE datatable;';
+		if($this->connection === false) {
+			exit('bağlantı yok');
+		}
+		$con = $this->connection;
+		//echo var_dump($con);exit();
+		if(($res=$con->query($sql))===false) {
+			echo $con->error;
+		}
+		else {
+			$row = $res->fetch_assoc();
+                        echo '<pre>';
+                        echo($row['Create Table']);
+                        //echo var_dump($row);
+                        echo '</pre>';
+		}
+        }
+        
+        function create_settings() {
+            $sql = 'CREATE TABLE `settings` (
+                    id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT
+                    ,`key` CHAR(50) NOT NULL
+                    ,`value` CHAR(255) NOT NULL
+                    ,PRIMARY KEY(id)
+                    )
+                    CHARSET=utf8 COLLATE utf8_general_ci ENGINE=InnoDB;
+                    ';
+		if($this->connection === false) {
+			exit('bağlantı yok');
+		}
+		$con = $this->connection;
+		//echo var_dump($con);exit();
+		if(($res=$con->query($sql))===false) {
+			echo $con->error;
+		}
+                else {
+                    echo 'settings tablosu oluşturuldu';
+                }
+        }
+        
+        
 	
 }
